4a47263b18304c77304f7f222cc7b44a469193d9
iambinhnpt <iambinhnpt@gmail.com>
2020-09-01 01:49:37 +0700

    Updates

---
+++ precache-manifest.129d5160cf8c9d41bb208ae9ec22cc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bef43d6424962071ddf5b9989b4258",
+    "url": "/e-learning/index.html"
+  },
+  {
+    "revision": "5ea04e9dd4769fad86bf",
+    "url": "/e-learning/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c34052aaa01303fb5796",
+    "url": "/e-learning/static/js/2.87dfc05c.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/e-learning/static/js/2.87dfc05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea04e9dd4769fad86bf",
+    "url": "/e-learning/static/js/main.cc1b767d.chunk.js"
+  },
+  {
+    "revision": "2ad998701ec7a7cf7d2f",
+    "url": "/e-learning/static/js/runtime-main.9815acf3.js"
+  }
+]);
